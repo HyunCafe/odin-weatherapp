a4784ec599f4669216f76715b440aeb19bcaa832
HyunCafe <eparksuncafe@gmail.com>
2023-04-21 12:44:10 -0500

    Add: Add API Logic, add SkyCons lib

---
+++ src/modules/api.js
@@ -0,0 +1,43 @@
+const apiKey = 'X5Sip7uKIkUGYS3EZUnarLLlYZRdRWAg';
+const searchQueryInput = document.getElementById('searchQuery');
+const searchButton = document.getElementById('searchButton');
+
+async function fetchWeatherData(searchQuery) {
+  try {
+    // First, fetch the location key based on the search query
+    const locationUrl = `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${apiKey}&q=${encodeURIComponent(searchQuery)}`;
+    const locationResponse = await fetch(locationUrl);
+
+    if (!locationResponse.ok) {
+      throw new Error('Network response was not ok');
+    }
+
+    const locations = await locationResponse.json();
+
+    if (locations.length === 0) {
+      console.error('No locations found for the search query');
+      return;
+    }
+
+    const locationKey = locations[0].Key;
+
+    // Now, fetch the weather data for the location key
+    const weatherUrl = `http://dataservice.accuweather.com/forecasts/v1/daily/1day/${locationKey}?apikey=${apiKey}`;
+    const weatherResponse = await fetch(weatherUrl);
+
+    if (!weatherResponse.ok) {
+      throw new Error('Network response was not ok');
+    }
+
+    const weatherData = await weatherResponse.json();
+    console.log(weatherData);
+  } catch (error) {
+    console.error('There was a problem with the fetch operation:', error);
+  }
+}
+
+searchButton.addEventListener('click', () => {
+    const searchQuery = searchQueryInput.value; // Get the user's input
+    fetchWeatherData(searchQuery);
+  });
+fetchWeatherData(searchQuery);
