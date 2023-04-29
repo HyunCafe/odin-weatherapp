4e118aedc40b31f6f6a5f1925541aa83f01c358d
HyunCafe <eparksuncafe@gmail.com>
2023-04-29 15:43:53 -0500

    Add: added icons to hourly as well

--- src/modules/hourly-forecast.js
+++ src/modules/hourly-forecast.js
@@ -1,23 +1,28 @@
 'use strict';
+import { getWeatherIcon } from "./weekly-forecast";
 
 export function updateHourlyForecast(hourlyData) {
-  const hourlyTimeline = document.querySelector('.hourly-forecast__timeline');
-  hourlyTimeline.innerHTML = '';
-
-  for (let i = 0; i < 12; i++) {
-    const forecast = hourlyData[i];
-    const time = new Date(forecast.DateTime).toLocaleTimeString('en-US', { hour: 'numeric' });
-    const condition = forecast.IconPhrase;
-    const degrees = Math.round(forecast.Temperature.Value);
-
-    const hourlyForecastItem = `
-      <div class="hourly-forecast__item">
-        <div class="hourly-forecast__time">${time}</div>
-        <div class="hourly-forecast__condition">${condition}</div>
-        <div class="hourly-forecast__degrees">${degrees}°</div>
-      </div>
-    `;
-
-    hourlyTimeline.insertAdjacentHTML('beforeend', hourlyForecastItem);
+    const hourlyTimeline = document.querySelector('.hourly-forecast__timeline');
+    hourlyTimeline.textContent = '';
+  
+    for (let i = 0; i < 12; i++) {
+      const forecast = hourlyData[i];
+      const time = new Date(forecast.DateTime).toLocaleTimeString('en-US', { hour: 'numeric' });
+      const condition = forecast.IconPhrase;
+      const iconClass = `wi ${getWeatherIcon(condition)}`;
+      const degrees = Math.round(forecast.Temperature.Value);
+  
+      const hourlyForecastItem = `
+        <div class="hourly-forecast__item">
+          <div class="hourly-forecast__time">${time}</div>
+          <div class="hourly-forecast__condition">
+            <span>${condition}</span> <br>
+            <i class="${iconClass}"></i>
+          </div>
+          <div class="hourly-forecast__degrees">${degrees}°</div>
+        </div>
+      `;
+  
+      hourlyTimeline.insertAdjacentHTML('beforeend', hourlyForecastItem);
+    }
   }
-}
